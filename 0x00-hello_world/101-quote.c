79407f8bec3a5d9b2ea58c4e6e802561e8bbdfd3
Hillary Cheserek <cheserekhillary@gmail.com>
2023-03-16 03:44:43 -0700

    Task number 8: a C program that prints exactly and that piece of art is useful" - Dora Korpar, 2015-10-19, followed by a new line, to the standard error.

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * main - outputs and that piece of art is useful - Dora Korpar, 2015-10-19
+ * Return: Exits with 0 as a success
+ */
+int main(void)
+{
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
+}
