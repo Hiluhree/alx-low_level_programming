c63963879a1c9e73bea7c5ce45e181e5c6f6679a
Hillary Cheserek <cheserekhillary@gmail.com>
2023-03-16 02:44:37 -0700

    Editing the file content

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,11 @@
 #include<stdio.h>
 
-/* 
- * main is the entry point for our program
- * return 0 should print success
+/** 
+ * main - is the entry point for our program
+ * Return: 0 should return success
  */
-int main(void){
+int main(void)
+{
 	puts("\"Programming is like building a multilingual puzzle");
-	return(0);
+	return (0);
 }
