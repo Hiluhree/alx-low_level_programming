d6094d8381f8dbe6c6b596f709c74219311f9048
Hillary Cheserek <cheserekhillary@gmail.com>
2023-03-17 07:26:12 -0700

    Redcuing the characters on comment for main

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 
 /**
- * main - a program that prints the alphabet in lowercase, followed by a new line.
+ * main - a program that prints the alphabet in lowercase,
+ * followed by a new line.
  * Return: returns a success
  */
 int main(void)
