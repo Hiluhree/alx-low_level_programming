0d199dcf4ea6ce094d226cedc86e3ad0b828d650
Hillary Cheserek <cheserekhillary@gmail.com>
2023-03-17 05:56:32 -0700

    Removing trailing whitespace on line 6

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,7 +3,7 @@
 #include <stdio.h>
 
 /**
- * main - assign a random number to the variable n 
+ * main - assign a random number to the variable n
  * each time it is executed.
  * Return: Always returns 0 as a success
  */
