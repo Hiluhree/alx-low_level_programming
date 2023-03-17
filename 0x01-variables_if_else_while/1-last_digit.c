8faf2619302698683230817279b84e129639db41
Hillary Cheserek <cheserekhillary@gmail.com>
2023-03-17 06:21:40 -0700

    Adding space to the first if and removing whitespace in line 6

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -3,7 +3,7 @@
 #include <stdio.h>
 
 /**
- * main - A program that assigns a random number to the variable 
+ * main - A program that assigns a random number to the variable
  * n each time it is executed.
  * Return: Exits with a success of 0
  */
@@ -13,7 +13,7 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	printf("Last digit of %d is %d", n, n % 10);
+	printf("Last digit of %d is %d ", n, n % 10);
 	if (n % 10 > 5)
 		printf("and is greater than 5\n");
 	else if (n % 10 == 0)
