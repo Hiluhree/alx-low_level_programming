5e0412317d1c2065fc37f850d1e2ea2727ee1513
Hillary Cheserek <cheserekhillary@gmail.com>
2023-03-23 23:01:36 -0700

    Going back to previous code

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -17,23 +17,19 @@ int main(void)
 		{
 			printf("FizzBuzz ");
 		}
-		else if (i % 3 == 0 && i % 5 != 0)
+		else if (i % 3 == 0)
 		{
 			printf("Fizz ");
 		}
-		else if (i % 5 == 0 && i % 3 != 0)
+		else if (i % 5 == 0)
 		{
 			printf("Buzz ");
 		}
-		else if (i == 1)
-		{
-			printf("%d", i);
-		}
 		else
 		{
-			printf(" %d", i);
+			printf("%d ", i);
 		}
-	}
+		{
 	printf("\n");
 	return (0);
 }
