f88f59343e84c07771d01fc28b5176d8bba7fece
Hillary Cheserek <cheserekhillary@gmail.com>
2023-03-27 12:17:41 -0700

    Task 11: a program that generates random valid passwords for the program 101-crackme.

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - a program that generates random valid
+ * passwords for the program 101-crackme.
+ * Return: returns 0
+ */
+int main(void)
+{
+	char password[96];
+	int i = 0, sum = 0, half1, half2;
+
+	srand(time(0));
+
+	while (sume < 2772)
+	{
+		password[i] = 33 + rand() % 94;
+		sum += password[i++];
+	}
+	password[i] = '\0';
+
+	if (sum != 2772)
+	{
+		half1 = (sum - 2772) / 2;
+		half2 = (sum - 2772) / 2;
+		if ((sum - 2772) % 2 != 0)
+		{
+			half1++;
+		}
+		for (i = 0; password[i]; i++)
+		{
+			if (password[i] >= (33 + half1))
+			{
+				password[i] -= half1;
+				break;
+			}
+		}
+		for (i = 0; password[i]; i++)
+		{
+			if (password[i] >= (33 + half2))
+			{
+				password[i] -= half2;
+				break;
+			}
+		}
+	}
+	printf("%s", password);
+	return (0);
+}
