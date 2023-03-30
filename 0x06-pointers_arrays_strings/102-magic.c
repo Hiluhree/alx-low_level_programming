75f384185821d093f8c67374f3f66d89520ae674
Hillary Cheserek <cheserekhillary@gmail.com>
2023-03-30 01:15:55 -0700

    Task 10: Adding line of code to the provided code so that the program prints a[2] = 98, followed by a new line

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,9 +1,5 @@
 #include <stdio.h>
 
-/**
- * main - to print 98
- * Return: Always 0
- */
 int main(void)
 {
 	int n;
@@ -23,5 +19,5 @@ int main(void)
 	*(p + 5) = 98;
 	/* ...so that this prints 98\n */
 	printf("a[2] = %d\n", a[2]);
-  return (0);
+	return (0);
 }
