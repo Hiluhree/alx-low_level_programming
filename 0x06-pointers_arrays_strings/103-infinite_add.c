0d15bd528b50330b1035b14a8c8b6d9db03c7863
Hillary Cheserek <cheserekhillary@gmail.com>
2023-03-30 02:45:58 -0700

    Task 11: a function that adds two numbers.

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,44 +1,35 @@
-#include "main.h"
-#include <stdlib.h>
-#include <string.h>
 
 /**
- * infinite_add - Adds two numbers.
- * @n1: The first number to be added.
- * @n2: The second number to be added.
- * @r: The buffer to store the result.
- * @size_r: The buffer size.
+ * infinite_add - a function that adds two numbers.
+ * @n1: The first number to be added
+ * @n2: The second number to be added
+ * @r: The buffer to store the result of the addition
+ * @size_r: The size of the buffer
  *
- * Return: If r can store the sum - a pointer to the result.
- *         If r cannot store the sum - 0.
+ * Return: pointer to the sum or the result
+ *  If r can store the sum - a pointer to the result.
+ *  If r cannot store the sum - 0
  */
-char* infinite_add(char* n1, char* n2, char* r, int size_r) {
-    int len1 = strlen(n1);
-    int len2 = strlen(n2);
-    int len = len1 > len2 ? len1 : len2; // get the maximum length
-    int carry = 0;
+char *infinite_add(char *n1, char *n2, char *r, int size_r)
+{
+        int i;
+        int len1 = 0;
+        int len2 = 0;
 
-    if (len + 1 > size_r) { // check if the result can fit in r
-        return 0;
-    }
-
-    r[len+1] = '\0'; // set the null terminator
-
-    for (int i = 0; i < len; i++) {
-        int d1 = i < len1 ? n1[len1-1-i] - '0' : 0; // get the digit from n1 or 0 if out of bounds
-        int d2 = i < len2 ? n2[len2-1-i] - '0' : 0; // get the digit from n2 or 0 if out of bounds
-        int sum = d1 + d2 + carry;
-        carry = sum / 10;
-        r[len-i] = sum % 10 + '0'; // store the digit in r
-    }
-
-    if (carry > 0) { // if there's a carry left, add it to the most significant digit
-    if (len + 2 > size_r) { // check if the result can fit in r
-            return 0;
+        for (i = 0; *(n1 + i); i++)
+        {
+                len1++;
         }
-        r[0] = carry + '0';
-        return r;
-    }
-
-    return r+1; // skip the leading 0 (if any) and return the result
+        for (i = 0; *(n2 + i); i++)
+        {
+                len2++;
+        }
+        if (size_r <= len1 + 1 || size_r <= len2 + 1)
+        {
+                return (0);
+        }
+        n1 += len1 - 1;
+        n2 += len2 - 1;
+        *(r + size_r) = '\0';
+        return (0);
 }
