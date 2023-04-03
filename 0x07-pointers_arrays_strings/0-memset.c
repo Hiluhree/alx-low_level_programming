58e8cd9e0c0da17f30f111b643e94965f31de47f
Hillary Cheserek <cheserekhillary@gmail.com>
2023-04-03 07:33:44 -0700

    Adding the descriptionfor the function

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,6 +1,6 @@
 #include "main.h"
 
-/*
+/**
  * _memset - a function that fills memory with a constant byte.
  * The function fills the first n bytes of the memory area
  * pointed to by s with the constant byte b
