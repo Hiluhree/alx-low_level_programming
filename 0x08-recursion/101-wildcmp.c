723c5acfa3364d3e820888e2940004c01890377b
Hillary Cheserek <cheserekhillary@gmail.com>
2023-04-05 22:58:53 -0700

    Correcting the typo

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -14,16 +14,18 @@ int wildcmp(char *s1, char *s2)
 	{
 		return (1);
 	}
-	else if (*s1 == *s2 || (*s2 == '*' && *(s2 + 1) != '\0'
-			&& wildcmp(s1 + 1, s2) == 1))
+	else if (*s2 == '*' && *(s2 + 1) != '\0' && *s1 == '\0')
 	{
-		return (wildcmp(s1 + 1, s2 + 1));
+		return (0);
 	}
 	else if (*s2 == '*')
 	{
 		return (wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2));
 	}
-	else
+	else if (*s1 == *s2)
+	{
+		return (wildcmp(s1 + 1, s2 + 1));
+	}
 	{
 
 		return (0);
