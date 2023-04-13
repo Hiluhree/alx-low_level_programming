21ef586d379ba58427762fb1655da2e69954aed6
Hillary Cheserek <cheserekhillary@gmail.com>
2023-04-13 07:16:58 -0700

    Task 6: a program that multiplies two positive numbers.

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -3,7 +3,7 @@
 #include <stdlib.h>
 
 /**
- * is_digits_only - checks if a string contains a non-digit char
+ * is_digit_only - checks if a string contains a non-digit char
  * @str: string to be evaluated
  *
  * Return: 0 if a non-digit is found, 1 otherwise
@@ -62,23 +62,18 @@ int main(int argc, char *argv[])
 	str1 = argv[1], str2 = argv[2];
 	if (argc != 3 || !is_digit_only(str1) || !is_digit_only(str2))
 		error_handler();
-
 	len1 = _strlen(str1);
 	len2 = _strlen(str2);
 	len = len1 + len2 + 1;
 	result = malloc(sizeof(int) * len);
-
 	if (!result)
 		return (1);
-
 	for (i = 0; i <= len1 + len2; i++)
 		result[i] = 0;
-
 	for (len1 = len1 - 1; len1 >= 0; len1--)
 	{
 		digit1 = str1[len1] - '0';
 		carry = 0;
-
 		for (len2 = _strlen(str2) - 1; len2 >= 0; len2--)
 		{
 			digit2 = str2[len2] - '0';
@@ -86,26 +81,19 @@ int main(int argc, char *argv[])
 			result[len1 + len2 + 1] = carry % 10;
 			carry /= 10;
 		}
-
 		if (carry > 0)
 			result[len1 + len2 + 1] += carry;
 	}
-
 	for (i = 0; i < len - 1; i++)
 	{
 		if (result[i])
 			a = 1;
-
 		if (a)
 			_putchar(result[i] + '0');
 	}
-
 	if (!a)
 		_putchar('0');
-
 	_putchar('\n');
-
 	free(result);
-
 	return (0);
 }
