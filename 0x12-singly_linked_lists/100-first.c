8af0ed12a6f6eb3013d3c0429e0c33437efbdd67
Hillary Cheserek <cheserekhillary@gmail.com>
2023-04-27 02:49:38 -0700

    Task 5:  a function that prints a quote

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * print_quote -  a function to print quote
+ */
+void print_quote(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
+
+/**
+ * main - a function to print quote
+ * Return: returns a 0 on success
+ */
+
+int hare_quote(void)
+{
+	print_quote();
+
+	return (0);
+}
