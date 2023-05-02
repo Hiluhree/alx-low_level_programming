ae0f7702a06dc22ad2c7ba3a46094911b1729680
Hillary Cheserek <cheserekhillary@gmail.com>
2023-05-02 04:11:39 -0700

    correction

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -6,15 +6,17 @@
  */
 void free_listint2(listint_t **head)
 {
-	listint_t *current = *head;
-	listint_t *next;
+	listint_t *current;
 
-	while (current != NULL)
+	if (head == NULL)
+		return;
+
+	while (*head)
 	{
-		next = current->next;
-		free(current);
-		current = next;
+		current = (*head)->next;
+		free(*head);
+		*head = current;
 	}
 
-	*head = NULL;
+	head = NULL;
 }
