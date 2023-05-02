57d9d41bc86fa3c8b61e246e5fcd9e34645b3e66
Hillary Cheserek <cheserekhillary@gmail.com>
2023-05-02 04:00:07 -0700

    Correcting the description for return

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -5,7 +5,7 @@
  * print_listint - a function that prints all the elements of a listint_t list
  * @h: A pointer to the head of the list
  *
- * Return: returns the number of the nodes
+ * Return: the number of the nodes
  */
 size_t print_listint(const listint_t *h)
 {
