88ea365b92e137290f2358b6ed9a9b5d2a9bd73a
Hillary Cheserek <cheserekhillary@gmail.com>
2023-05-04 02:24:52 -0700

    Making a little correction on the return value

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -2,15 +2,15 @@
 #include <limits.h>
 /**
  * clear_bit - a function that sets the value of a bit to 0 at a given index
- * @n: number
- * @index: index
- * Return: returns
+ * @n: pointer to the bit
+ * @index: index to set the value at
+ * Return: returns -1 if an error occurs, otherwise returns 1
  */
 int clear_bit(unsigned long int *n, unsigned int index)
 {
 	if (index >= sizeof(unsigned long int) * CHAR_BIT)
 	{
-		return (1);
+		return (-1);
 	}
 
 	*n &= ~(1UL << index);
