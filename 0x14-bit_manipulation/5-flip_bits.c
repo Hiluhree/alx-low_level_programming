69da8d61be66901a9f52ec471f343a799ab4d2d3
Hillary Cheserek <cheserekhillary@gmail.com>
2023-05-04 02:27:15 -0700

    Making corrections for the description

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -3,9 +3,9 @@
 /**
  * flip_bits - a function that returns the number of bits
  * you would need to flip to get from one number to another
- * @n: first number
- * @m: second number
- * Return: returns the cpount
+ * @n: the number
+ * @m: the number to flip to
+ * Return: returns the necessary number of bitd to flip to get from n to m
  */
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
