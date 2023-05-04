3a2e262c9833ac23beab2e82cbce386bad94397e
Hillary Cheserek <cheserekhillary@gmail.com>
2023-05-04 01:44:17 -0700

    Task 5: a function that returns the number of bits you would need to flip to get from one number to another

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * flip_bits - a function that returns the number of bits
+ * you would need to flip to get from one number to another
+ * @n: first number
+ * @m: second number
+ * Return: returns the cpount
+ */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned long int diff =  n ^ m;
+	unsigned int count = 0;
+
+	while (diff != 0)
+	{
+		count += diff & 1;
+		diff >>= 1;
+	}
+
+	return (count);
+}
